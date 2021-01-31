a66e719c15d3297b087122c7b03c610bb242c673
Cat-Denton <willwdenton@gmail.com>
2021-01-31 12:18:36 -0800

    Store inputs as array and pop up the array in the browser.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,11 @@
 $(document).ready(function() {
-  
-})
+  $("#favThings").submit(function(event) {
+    const animal = $("input#animal").val();
+    const food = $("input#food").val();
+    const color = $("input#color").val();
+    
+    let favThings = [animal,food,color];
+    alert(favThings);
+    event.preventDefault();
+  });
+});
